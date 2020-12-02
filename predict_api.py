1f118fbb060aee1a629f782216262574f67259e1
Gnosis <gaojing1996@vip.qq.com>
2020-12-02 16:22:20 +0800

    opt load model method.

--- predict_api.py
+++ predict_api.py
@@ -9,21 +9,7 @@ from flask import Flask
 from get_train_data import get_current_number, spider
 from tensorflow.keras.models import load_model
 
-# load model
-model_red_1 = load_model('model/lstm_model_红球号码_1.h5')
-model_red_2 = load_model('model/lstm_model_红球号码_2.h5')
-model_red_3 = load_model('model/lstm_model_红球号码_3.h5')
-model_red_4 = load_model('model/lstm_model_红球号码_4.h5')
-model_red_5 = load_model('model/lstm_model_红球号码_5.h5')
-model_red_6 = load_model('model/lstm_model_红球号码_6.h5')
-model_blue = load_model('model/lstm_model_蓝球.h5')
-model_list = [
-    model_red_1, model_red_2, model_red_3, model_red_4, model_red_5, model_red_6, model_blue
-]
-print("[INFO] 模型加载成功！")
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def main():
@@ -33,6 +19,7 @@ def main():
 @app.route('/predict_api', methods=['GET'])
 def get_predict_result():
     data = spider(str(int(get_current_number()) - 2), get_current_number(), "predict")[BOLL_NAME]
+    model_list = load_model()
     result = []
     for i, model in enumerate(model_list):
         boll_name = BOLL_NAME[i]
@@ -40,6 +27,21 @@ def get_predict_result():
         p_data = np.array(data_list).reshape([-1, 3, 1]).astype(np.float32)
         result.extend(model.predict_classes(p_data))
     return json.dumps({b_name: int(res) + 1 for b_name, res in zip(BOLL_NAME, result)})
+
+# load model
+def load_model():
+    model_red_1 = load_model('model/lstm_model_红球号码_1.h5')
+    model_red_2 = load_model('model/lstm_model_红球号码_2.h5')
+    model_red_3 = load_model('model/lstm_model_红球号码_3.h5')
+    model_red_4 = load_model('model/lstm_model_红球号码_4.h5')
+    model_red_5 = load_model('model/lstm_model_红球号码_5.h5')
+    model_red_6 = load_model('model/lstm_model_红球号码_6.h5')
+    model_blue = load_model('model/lstm_model_蓝球.h5')
+    model_list = [
+        model_red_1, model_red_2, model_red_3, model_red_4, model_red_5, model_red_6, model_blue
+    ]
+    print("[INFO] 模型加载成功！")
+    return model_list
 
 
 if __name__ == '__main__':
